0a97d145900e51ffe5b3ff3437aea331bf5ea63a
OMFGNinja <marrietag98@gmail.com>
2022-03-26 00:35:43 -0400

    Added a simple UI to the rock paper scissors game

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
 let computerSelection, playerSelection, playerScore = 0, computerScore = 0;
 
-function computerPlay() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    switch (randomNumber){
-        case 0:
-            return 'rock'
-        case 1:
-            return 'scissors'
-        case 2:
-            return 'paper'
-    }
-}
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
+
+rock.addEventListener('click',randomChoices)
+paper.addEventListener('click',randomChoices)
+scissors.addEventListener('click',randomChoices)
+
+const results = document.querySelector('#results')
+const final = document.querySelector('#final')
 
 function randomChoices(){
     computerSelection = computerPlay()
-    let playerSelection2 = prompt('What will you use?')
-    playerSelection = playerSelection2.toLowerCase()
-    roundPlay (computerSelection, playerSelection)
+    function computerPlay() {
+        let randomNumber = Math.floor(Math.random() * 3)
+        switch (randomNumber){
+            case 0:
+                return 'rock'
+            case 1:
+                return 'scissors'
+            case 2:
+                return 'paper'
+        }
+    }
+    roundPlay (computerSelection, this.id)
 }
 
 function roundPlay(a, b){
-    console.log(a)
-    console.log(b)
-    if (a==b){
-        console.log('Its a tie!')
-        playerScore = playerScore + 1
-        computerScore = computerScore + 1
-        console.log(playerScore,computerScore)
+    if (b==a){
+        results.textContent = `It's a tie! The score right now is: Computer: ${computerScore} , You: ${playerScore} .`
+        final.textContent = ` `
     }
     else if ((b=='rock' && a=='scissors')||(b=='paper' && a=='rock')||(b=='scissors' && a=='paper')){
-        console.log('You win!')
         playerScore = playerScore + 1
-        console.log(playerScore,computerScore)
+        results.textContent = `You win! ${b} beats ${a}! The score right now is: Computer: ${computerScore} , You: ${playerScore} .`
+        final.textContent = ` `
     }
     else {
-        console.log('You lose!')
         computerScore = computerScore + 1
-        console.log(playerScore,computerScore)
+        results.textContent = `You lose! ${a} beats ${b}! The score right now is: Computer: ${computerScore} , You: ${playerScore} .`
+        final.textContent = ` `
+    }
+    if (playerScore == 5){
+        final.textContent = `You win the game! With a total of: ${playerScore} points! The computer had: ${computerScore} points!`
+        playerScore = 0
+        computerScore = 0
+    }
+    else if (computerScore == 5){
+        final.textContent = `You lose the game! The computer beat you with a total of: ${computerScore} points! You had: ${playerScore} points!`
+        playerScore = 0
+        computerScore = 0
     }
 }
-
-function game(){
-    for (let i=1; i<=5; i++){
-        randomChoices()
-    }
-    if (playerScore==computerScore){console.log('Nobody wins!')}
-    else if (playerScore>computerScore){console.log('You are the winner of the game!')}
-    else {console.log('You lost to the computer!')}
-}
-
-game();
